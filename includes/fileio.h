92bfce1a58c278b2b470fb63268210199a9a0079
samithchowdhury <43484916+samithchowdhury@users.noreply.github.com>
2020-09-25 22:17:32 -0400

    add files via upload

---
+++ includes/fileio.h
@@ -0,0 +1,16 @@
+#ifndef FILEIO_H
+#define FILEIO_H
+#include <iostream>
+#include <string>
+#include <vector>
+#include "../includes/constants.h"
+
+//attempt to open file 'filename' and read in all data
+//returns SUCCESS if all goes well or COULD_NOT_OPEN_FILE
+int loadData(const std::string filename, std::vector<process> &myProcesses);
+
+//attempt to create or open file 'filename' to write all data to
+//returns SUCCESS if all goes well or COULD_NOT_OPEN_FILE
+int saveData(const std::string filename, std::vector<process> &myProcesses);
+
+#endif 
